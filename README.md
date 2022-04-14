b3712553e47ba860dfa47847f14b1e7b21b2de01
Samuel Anthony <51282543+samdsg@users.noreply.github.com>
2022-04-14 09:08:46 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# AngularTask
+
+Angular Task Tracker
+
+## Development server
+
+    ```Json-server```
+
+## To Install?
+`git clone [Repo link]`
+`npm install`
+
+## Run applicaiton
+`ng serve`
+
+## Run Backend Server
+`npm run serve`
+
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
+import { Task } from '../../Task';
+// import { TASKS } from "../../mock-taks";
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  // tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    // this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((t) => this.tasks.push(t));
+  }
+
+}
